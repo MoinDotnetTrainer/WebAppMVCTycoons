9820a7739439a0091cc21f4f0df48eca8f602de2
moinkmu <m.a.khaja.moinuddin@accenture.com>
2026-09-03 15:10:20 +0530

    Curd

--- WebAppMVCTycoons/Controllers/BooksController.cs
+++ WebAppMVCTycoons/Controllers/BooksController.cs
@@ -10,7 +10,7 @@ namespace WebAppMVCTycoons.Controllers
         public BooksController()
         {
             db = new AppDb();
-        }       
+        }
         [HttpGet]
         public IActionResult AddBooks()
         {
@@ -24,7 +24,81 @@ namespace WebAppMVCTycoons.Controllers
             // Entity Framework Core
 
             db.books.Add(data); // Add --> Insert ops
-            db.SaveChanges(); // commit changes to db , exe insert query
+            int res = db.SaveChanges(); // commit changes to db , exe insert query
+
+            if (res > 0)
+            {
+                return RedirectToAction("GetBooks");
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+
+            // get the data from db
+            // ef core is also uses linq syntax's
+            // LINQ --> Get the data
+            IEnumerable<BooksModel> data = from s in db.books select s;
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult EditBooks(int BookID)
+        {
+            // we in db , get here and binds to model
+
+            BooksModel data = (from s in db.books where s.BookID == BookID select s).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult EditBooks(BooksModel data)
+        {
+            db.books.Update(data);
+            int res = db.SaveChanges();
+            if (res > 0)
+            {
+                return RedirectToAction("GetBooks");
+            }
+            return View();
+        }
+        
+
+        [HttpGet]
+        public IActionResult BookDetails(int BookID)
+        {
+            // we in db , get here and binds to model
+
+            BooksModel data = (from s in db.books where s.BookID == BookID select s).FirstOrDefault();
+            return View(data);
+        }
+
+
+        [HttpGet]
+        public IActionResult DeleteBook(int BookID)
+        {
+            // we in db , get here and binds to model
+
+            BooksModel data = (from s in db.books where s.BookID == BookID select s).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost,ActionName("DeleteBook")]
+        public IActionResult DeleteConfirm(int BookID)
+        {
+            var find = db.books.Find(BookID);
+            if (find != null)
+            {
+                db.books.Remove(find);
+                int res = db.SaveChanges();
+                if (res > 0)
+                {
+                    return RedirectToAction("GetBooks");
+                }
+            }
             return View();
         }
     }
